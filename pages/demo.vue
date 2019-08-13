933a4cfdad34c0ece762c580a7301a5a76f3ed72
cherish <673010805@qq.com>
2019-08-13 19:06:06 +0800

    创建了nuxt项目

---
+++ pages/demo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>{{ name }}</div>
+    <div>{{ name }}</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: 'cherish',
+      objects: [{ id: 1 }, { id: 1 }, { id: 1 }],
+      names: 123
+    }
+  },
+  methods: {
+    haha() {
+      const data = this.objects.map(item => item)
+      console.log(data)
+    }
+  }
+}
+</script>
